d22f4d92630aef39c3c1c7b2c391b25cc0994153
quangnvo <voquang.usth@gmail.com>
2024-03-29 05:18:35 +0100

    update create-work

--- app/create-work/page.jsx
+++ app/create-work/page.jsx
@@ -3,12 +3,17 @@
 import { useState } from "react"
 import Form from "@components/Form"
 import Navbar from "@components/Navbar"
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const CreateWork = () => {
 
 	/*################
 	# VARIABLES
 	################*/
+	const { data: session } = useSession()
+	const router = useRouter()
+
 	const [work, setWork] = useState({
 		creator: "",
 		category: "",
@@ -25,9 +30,36 @@ const CreateWork = () => {
 	# FUNCTIONS
 	################*/
 
-	// This handleSubmit function will be passed to the "Form" component as a prop.
-	const handleSubmit = (e) => {
+	if (session) {
+		work.creator = session?.user?._id
+	}
 
+	// This handleSubmit function will be passed to the "Form" component as a prop.
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			const newWorkForm = new FormData()
+
+			for (var key in work) {
+				newWorkForm.append(key, work[key])
+			}
+
+			work.photos.forEach((photo) => {
+				newWorkForm.append("workPhotoPaths", photo)
+			})
+
+			const response = await fetch("/api/work/new", {
+				method: "POST",
+				body: newWorkForm
+			})
+
+			if (response.ok) {
+				router.push(`/shop?id=${session?.user?._id}`)
+			}
+		} catch (err) {
+			console.log("Publish Work failed", err.message)
+		}
 	}
 
 	/*################
